3ea5f01b71a842ed2f40e5dabf6502d7268f7915
Gustavo <henriquesgustavo138@gmail.com>
2025-05-03 16:55:47 -0300

    iniciando pagina de negocios

---
+++ negocios.js
@@ -0,0 +1,9 @@
+let somatorio = []
+
+for(let i =1 ; i<=100;i++){
+    somatorio.push(i)
+}
+
+const fazerSomatorio = vetorPserSomado => vetorPserSomado.reduce((atual,item)=> atual + item)
+
+console.log(fazerSomatorio(somatorio))
